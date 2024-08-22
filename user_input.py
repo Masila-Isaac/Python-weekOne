5de57fb42aef1db4209677f8a80db932782ec1bf
Masila-Isaac <masilaisaacmim@gmail.com>
2024-08-22 21:12:43 +0300

    user_input

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Ask the user for their name, age, and location
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where do you live? ")
+
+# Print out a message
+print(f"How are you {name}, you are {age} years old and live in {location}.")
